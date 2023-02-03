b559fc701ed92a381d158421e92a77ba06e6dcf7
Andrew <Kusnir8655@gmil.com>
2023-02-03 13:09:14 +0100

    Modifying js

--- js/main.js
+++ js/main.js
@@ -1,16 +1,24 @@
 const nav = document.querySelector('.nav');
 const burgerBtn = document.querySelector('.burger-btn');
 const allNavItem = document.querySelectorAll('.nav__item');
+const navBtnBars = document.querySelector('.burger-btn__bars');
+const allSections = document.querySelectorAll('.section');
+const footerYear = document.querySelector('.footer__year');
+const body = document.querySelector('body');
 
 const navActive = () => {
     nav.classList.toggle('nav--active')
 
+
     allNavItem.forEach(item => {
         item.addEventListener('click', () => {
             nav.classList.remove('nav--active')
+            bodyHidden()
         })
     })
     handlNavItemAnimation();
+    navBtnBars.classList.remove('black-bars-colors')
+    bodyHidden();
 }
 
 const handlNavItemAnimation = () => {
@@ -24,7 +32,35 @@ const handlNavItemAnimation = () => {
 }
 
 
+const bodyHidden = () => {
+    if (nav.classList.contains('nav--active')) {
+        body.style.overflow = 'hidden'
+        navBtnBars.classList.remove('black-bars-color')
+    } else if (!nav.classList.contains('nav--active')) {
+        body.style.overflow = 'auto'
+    }
+}
+
+const currentYear = () => {
+
+    const today = new Date();
+    footerYear.textContent = today.getFullYear();
+
+}
+
+const burgerBtnColChanger = () => {
+    const currentSection = window.scrollY;
+    allSections.forEach(element => {
+        if (element.classList.contains('white-section') && element.offsetTop <= currentSection + 60) {
+            navBtnBars.classList.add('black-bars-colors')
+        } else if (!element.classList.contains('white-section') && element.offsetTop <= currentSection + 60) {
+            navBtnBars.classList.remove('black-bars-colors')
+        }
+    });
+}
 
 
-
+currentYear();
 burgerBtn.addEventListener('click', navActive);
+window.addEventListener('scroll', burgerBtnColChanger);
+// burgerBtn.addEventListener('click', bodyHidden);
